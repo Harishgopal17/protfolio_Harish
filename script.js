91efb61c76ef6a4a3d89cfbd92a6092ae82ddc81
Harishgopal17 <Harishgopal1705@gmail.com>
2024-02-06 07:55:29 +0530

    Updated meta tag and added the functionality to recive the email from contact form

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$("#submit-form").submit((e) => {
+    e.preventDefault()
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbyvCeqGX_IkJ0J1Kwwk8O-8Au59aZWxYXFtd4l-o-IFHuH_7VTt00gdiS2l8mb4cbDR/exec",
+        data: $("#submit-form").serialize(),
+        method: "post",
+        success: function (response) {
+            alert("Thank you")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error: function (err) {
+            alert("Something Error")
+
+        }
+    })
+})
+
 const text = document.querySelector(".sec-text");
 
 const textLoad = () => {
@@ -17,3 +35,5 @@ const textLoad = () => {
 
 textLoad();
 setInterval(textLoad, 16000);
+
+
