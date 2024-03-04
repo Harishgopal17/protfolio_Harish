1151e4698f7e367e50fefaa3dcabfe28d3881255
Harishgopal17 <Harishgopal1705@gmail.com>
2024-03-04 21:47:17 +0530

    removed alert on form

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 $("#submit-form").submit((e) => {
-    e.preventDefault()
-    $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbyvCeqGX_IkJ0J1Kwwk8O-8Au59aZWxYXFtd4l-o-IFHuH_7VTt00gdiS2l8mb4cbDR/exec",
-        data: $("#submit-form").serialize(),
-        method: "post",
-        success: function (response) {
-            alert("Thank you")
-            window.location.reload()
-            //window.location.href="https://google.com"
-        },
-        error: function (err) {
-            alert("Something Error")
-
-        }
-    })
-})
+  e.preventDefault();
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbyvCeqGX_IkJ0J1Kwwk8O-8Au59aZWxYXFtd4l-o-IFHuH_7VTt00gdiS2l8mb4cbDR/exec",
+    data: $("#submit-form").serialize(),
+    method: "post",
+    success: function (response) {
+      window.location.reload();
+      //window.location.href="https://google.com"
+    },
+    error: function (err) {
+      alert("Something Error");
+    },
+  });
+});
 
 const text = document.querySelector(".sec-text");
 
 const textLoad = () => {
-    setTimeout(() => {
-        text.textContent = "Everything";
-    }, 0);
-    setTimeout(() => {
-        text.textContent = "Love";
-    }, 4000);
-    setTimeout(() => {
-        text.textContent = "Addiction";
-    }, 8000);
-    setTimeout(() => {
-        text.textContent = "Fun";
-    }, 12000);
+  setTimeout(() => {
+    text.textContent = "Everything";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Love";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Addiction";
+  }, 8000);
+  setTimeout(() => {
+    text.textContent = "Fun";
+  }, 12000);
 };
 
 textLoad();
 setInterval(textLoad, 16000);
-
-
